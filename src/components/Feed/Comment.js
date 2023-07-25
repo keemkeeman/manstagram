56e9224e68fd6369e512f4aae999e0b722285feb
keemkeeman <manki9825@gmail.com>
2023-07-25 17:41:46 +0900

    230725-5

--- src/components/Feed/Comment.js
+++ src/components/Feed/Comment.js
@@ -3,12 +3,15 @@ import styles from "./Comment.module.css";
 const Comment = ({ comment }) => {
   return (
     <div className={styles.wrap}>
-      <span>
+      <span className={styles.innerWrap}>
         <span onClick={null} className={styles.nickName}>
           {comment.nickName}
         </span>
         <span className={styles.commentText}>{comment.commentText}</span>
       </span>
+      <div className={styles.editIcon}>
+        <i className="fa-solid fa-ellipsis"></i>
+      </div>
     </div>
   );
 };
