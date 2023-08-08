e915a40d40621e3d08076896b013d4d139f7c506
codingboxer <manki9825@gmail.com>
2023-08-08 21:15:52 +0900

    env setting

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "manstagram-77636.firebaseapp.com",
+  projectId: "manstagram-77636",
+  storageBucket: "manstagram-77636.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
